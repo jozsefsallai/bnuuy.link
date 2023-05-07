24e424055bb882c7b95a18f50f978605434d70e3
József Sallai <jozsef@sallai.me>
2023-05-07 23:15:05 +0300

    light theme and viewport sizing fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import LinkForm from "@/components/LinkForm";
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex flex-col items-center justify-center min-h-screen p-2">
+    <main className="flex items-center justify-center h-full">
+      <div className="flex flex-col items-center justify-center p-2">
         <Image
           src="/rabbit.svg"
           alt="bnuuy.link"
@@ -16,7 +16,7 @@ export default function Home() {
 
         <h1 className="text-5xl md:text-6xl font-bold mt-10">bnuuy.link</h1>
 
-        <p className="mt-3 text-sm text-gray-300 mb-10">
+        <p className="mt-3 text-sm text-gray-400 mb-10">
           <span className="text-blue-600">bnuuy.link</span> is a tiny, fast URL
           shortener running on the edge.
         </p>
